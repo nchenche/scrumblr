8131aaf3f095eea15751329922714d2332eef9dc
nchenche <nicolas.chevrollier@laposte.net>
2024-06-19 12:20:00 +0200

    add action to gridjs table

--- static/js/table/grid.js
+++ static/js/table/grid.js
@@ -57,10 +57,11 @@ const grid = new gridjs.Grid({
             formatter: (_, row) => {
                 const status = row.cells[2].data === "owner" ? '' : 'disabled';
                 const opacity = row.cells[2].data === "owner" ? '' : 'opacity-50';
+                const room = row.cells[0].data;
 
                 return gridjs.html(`
                     <div class="flex justify-between bg-transparent">
-                        <button ${status} class="${opacity}">
+                        <button id="delete-room" ${status} class="${opacity}" data-room=${room}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-4 h-4">
                                 <path d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z"></path>
                             </svg>
@@ -132,5 +133,16 @@ document.getElementById('wrapper').addEventListener('click', function (e) {
                   `;
         }
     }
+
+    if ( e.target && e.target.closest('#delete-room')) {
+
+        const room = e.target.closest('#delete-room').getAttribute('data-room');
+
+        console.log(room);
+
+    }
 });
+
+
+
 
