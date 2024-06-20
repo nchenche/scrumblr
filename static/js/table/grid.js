de699b01e5ea0a568604944e6eed7ae3356c3c57
nchenche <nicolas.chevrollier@laposte.net>
2024-06-20 14:53:36 +0200

    update info about grid js in utils

--- static/js/table/grid.js
+++ static/js/table/grid.js
@@ -1,13 +1,3 @@
-/*
-Gridjs plugin imported from head.ejs with the following lines:
-
-<script src="/lib/gridjs/dist/gridjs.umd.js"></script>
-<link href="/lib/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" type='text/css'/>
-
-and custom css to override default gridsj one.
-
-*/
-
 import { initializeGrid } from './utils.js'
 
 // Initialize grid on page load
